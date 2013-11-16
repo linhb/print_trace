a6af0f89e250ad01a778cab45cdec2e25f323dd0
Linh Bui <linhdbuivn@gmail.com>
2013-11-15 21:50:07 -0500

    nicer printouts

--- lib/print_trace.rb
+++ lib/print_trace.rb
@@ -1,7 +1,9 @@
 Signal.trap("USR1") do
+  trace = Thread.current.backtrace.join("\n")
+  puts trace
   File.open(File.join(Rails.root, "log", "print_trace.log"), "a") do |f|
-    f.write(Time.now)
-    f.write(Thread.current.backtrace.join("\n"))    
+    f.write("****** #{Time.now} ******")
+    f.write(trace)    
   end
 end
 def print_trace(pid)
