99a54537b6748c2fc99c87e1a6e2b64a3810ab6e
Linh Bui <linhdbuivn@gmail.com>
2013-11-16 19:42:21 -0500

    print local and instance variables

--- lib/print_trace.rb
+++ lib/print_trace.rb
@@ -1,10 +1,22 @@
 Signal.trap("USR1") do
+  get_trace
+end
+def get_trace
   trace = Thread.current.backtrace.join("\n")
-  puts trace
   File.open(File.join(Rails.root, "log", "print_trace.log"), "a") do |f|
     f.write("****** #{Time.now} ******\n")
+    f.write("Instance variables:\n")
+    instance_variables.each do |v| 
+      f.write("#{v}:\t#{eval(v.to_s).inspect}\n")
+    end
+    f.write("Local variables:\n")
+    local_variables.each do |v| 
+      f.write("#{v}:\t#{eval(v.to_s).inspect}\n")
+    end
+    f.write("Backtrace:\n")
     f.write(trace)    
-  end
+  end    
+  trace
 end
 def print_trace(pid)
   Process.kill("USR1", pid)
